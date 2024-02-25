8d6b95cf976cc98a3051eaf61c61fdba5c6b5f03
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-02-25 01:28:28 -0800

    completed hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import './App.scss';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import VideoPlayListItem from './components/VideoPlayListItem/VideoPlayListItem';
+import VideoDetails from './components/VideoDetails/VideoDetails';
+
+//import videoDetail from './data/video-details.json';
+import videoList from './data/video-details.json';
+
+import {useState} from 'react';
 
 function App() {
+
+   // const[videoDetail, getVideoDetail] = useState(videoDetail);
+    const[videoSelected, getActivedVideo] = useState(videoList[0]);
+    console.log(videoSelected);
+
   return(
+    <main>
     <Header />
+    <VideoPlayListItem videoSelected = {videoSelected} />
+    <div>
+        <div>
+            <VideoDetails videoDescription={videoSelected} />
+        </div>
+    </div>
+
+    </main>
   );
 }
 
---
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -0,0 +1,55 @@
+import view from '../../assets/Icons/views.svg';
+import like from '../../assets/Icons/likes.svg';
+import '../VideoDetails/VideoDetails.scss';
+
+function VideoDetails({videoDescription}) {
+//    // const timestamp = props.videoSelected.timestamp;
+//     let date = new Date(props.videoSelected.timestamp);
+//     const newDate = date.toLocaleDateString("en-US");
+
+//     console.log(newDate);
+const{
+    title,
+    channel,
+    description,
+    views,
+    likes,
+    timestamp
+} = videoDescription;
+
+    return (
+
+        <section className='video'>
+            <h3 className='video__title'> {title}</h3>
+            <div className='video__info'>
+                <div className='video__info--right'>
+                    <h1 className='video__info--channel'>
+                        By {channel}
+                    </h1>
+                    <span className='video__info--date video__info--subtitle'>{new Date(timestamp).toLocaleDateString('en-US')}</span>
+                </div>
+                
+                <div className='video__info--left'>
+                    <div>
+                    <img className='video__views' src={view} alt="icon" />
+                    <span className='video__info--subtitle video__info--num'>
+                        {views}
+                    </span>
+                    </div>
+                    <div>
+                    <img className='video__likes' src={like} alt="icon" />
+                    <span className='video__info--subtitle video__info--nums'>
+                        {likes}
+                    </span>
+                    </div>
+                </div>
+            </div>
+
+            <p className = 'video__description'>
+                {description}
+            </p>
+        </section>
+    );
+}
+
+export default VideoDetails;
