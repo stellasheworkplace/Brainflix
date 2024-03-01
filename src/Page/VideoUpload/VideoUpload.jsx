6fa04852c2eeca1aa16827e93c54f9939645136b
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-02-29 18:56:47 -0800

    updated Header Component

---
+++ src/Page/VideoUpload/VideoUpload.jsx
@@ -0,0 +1,14 @@
+import Header from'../../components/Header/Header';
+import UploadVideo from '../../components/UploadVideo/UploadVideo';
+
+function VideoUpload(){
+
+    return(
+        <>
+        <Header />
+        <UploadVideo />
+        </>
+    );
+}
+
+export default VideoUpload;
---
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -0,0 +1,7 @@
+
+
+function UploadVideo(){
+
+}
+
+export default UploadVideo;
