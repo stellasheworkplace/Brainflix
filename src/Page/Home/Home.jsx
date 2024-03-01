193ba9029954c88b1933cba3b06f489a48b180fa
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-02-29 22:30:45 -0800

    fetch all data to home page

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,9 +1,51 @@
 import Header from '../../components/Header/Header';
+import VideoPlayListItem from '../../components/VideoPlayListItem/VideoPlayListItem';
 
-function Home(){
-    return(
-        <Header />
-    );
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+
+function Home() {
+    const [videoSelected, setActivedVideo] = useState([]);
+    const [isLoadinng, setIsLoading] = useState(true);
+    const [hadError, setHasError] = useState(false);
+
+    useEffect(() => {
+        const API_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28";
+
+        const fetchVideos = async () => {
+            try {
+                const response = await axios.get(API_URL);
+
+                setIsLoading(false);
+                setActivedVideo(response.data);
+            } catch (error) {
+                setIsLoading(false);
+                setHasError(true);
+            }
+        };
+        fetchVideos();
+    } , []);
+
+return (
+    <main>
+    <Header />
+     <VideoPlayListItem videoSelected={videoSelected} />
+
+    {/* //<div className='app__bottom'>
+    //         <div className='app__box'>
+    //             <VideoDetails videoDescription={videoSelected} />
+    //             <Comment videoList={videoList} videoSelected={videoSelected} />
+    //         </div>
+
+    //         <div className='app__list'>
+    //             <VideoList videoList={videoList} videoSelected={videoSelected} />
+    //         </div>
+    //     </div> */}
+    </main>
+
+
+);
 }
 
 export default Home;
--- src/components/VideoPlayListItem/VideoPlayListItem.jsx
+++ src/components/VideoPlayListItem/VideoPlayListItem.jsx
@@ -4,17 +4,15 @@ function VideoPlayListItem(props) {
     let videoImage = props.videoSelected.image;
     let videoPlayer = props.videoSelected.video;
 
-    console.log(videoImage);
-    console.log(props);
 
     return (
-        <main>
+        <section>
             <div className="main">
                 <video width="100%" height="100%" poster={videoImage} controls>
                     <source src={videoPlayer}  className="main__video" alt='Playing Video' />
                 </video>
             </div>
-        </main>
+        </section>
     )
 }
 
