a5645ae0fa1c101dc441575e0cac4066560c0e25
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-03-02 16:58:47 -0800

    updated comment

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -5,27 +5,30 @@ import Comment from '../../components/Comment/Comment';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 
 
 function Home() {
-    const [videoSelected, setActivedVideo] = useState({});
     const [videoInfo, setVideoInfo] = useState([]);
+    const [videoSelected, setActivedVideo] = useState({});
+    
     const params = useParams();
 
     const [isLoadinng, setIsLoading] = useState(true);
     const [hadError, setHasError] = useState(false);
 
+    
+    
+   // const API_FIRST =  "https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${parmas.id}?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28";
     //fetch all video data
     useEffect(() => {
         const API_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28";
-       
-       
         const fetchVideos = async () => {
             try {
                 const response = await axios.get(API_URL);
-
+                
                 setIsLoading(false);
-                setActivedVideo(response.data);
+                setVideoInfo(response.data);
             } catch (error) {
                 setIsLoading(false);
                 setHasError(true);
@@ -36,27 +39,34 @@ function Home() {
 
     //fetch single video
     useEffect(() => {
-        const API_IDURL = "https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${parmas.id}?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28";
-
+        const API_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28";
+        
         const fetchSingleVideo = async () => {
             try {
-                const selectedResponse = await axios.get(API_IDURL);
-                setIsLoading(false);
-                setVideoInfo(response.data);
+                if (!params.id) {
+                    const response = await axios.get(API_URL);
+                    const selectedResponse = await axios.get(`https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${response.data[0].id}?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28`);
+                    
+                    setIsLoading(false);
+                    setActivedVideo(selectedResponse.data);
+                } else {
+                    const selectedResponse = await axios.get(`https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${params.id}?api_key=861ebcf7-3b96-48cc-9e68-da2d9a961d28`);
+                    setActivedVideo(selectedResponse);
+
+                }
             } catch (error) {
                 setIsLoading(false);
                 setHasError(true);
             }
-
         };
         fetchSingleVideo();
-    },[params.id]);
+    }, [params.id]);
 
     return (
         <main>
             <Header />
             <VideoPlayListItem videoSelected={videoSelected} />
-            <Comment commentInfo={videoInfo} videoSelected={videoSelected} />
+            <Comment comments={videoInfo} videoSelected={videoSelected} />
 
 
             {/* //<div className='app__bottom'>
--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -4,54 +4,54 @@ import avatar from '../../assets/Images/Mohan-muruge.jpg';
 import icon from '../../assets/Icons/add_comment.svg';
 import grey from '../../assets/Images/icon-like.jpeg';
 
-function Comment({ videoSelected, commentInfo }) {
+function Comment({ videoSelected }) {
     return (
-        <div className='comment'>
-            <h3 className="commennt__title">3 Comments</h3>
-            <div className='comment__upload'>
-                <div className='comment__right'>
-                    <img className="comment__image" src={avatar} alt="avater image" />
-                </div>
+        <>
+            <div className='comment'>
+                <h3 className="commennt__title">3 Comments</h3>
+                <div className='comment__upload'>
+                    <div className='comment__right'>
+                        <img className="comment__image" src={avatar} alt="avater image" />
+                    </div>
 
-                <div className='comment__left'>
-                    <h2>JOIN THE CONVERSATION</h2>
-                    <form className='comment__form'>
-                        <div className='comment__box'>
-                            <input className='comment__input' type="text" placeholder='Add a new comment' name="comment" />
-                        </div>
-
-                        <div className='comment__button'>
-                            <button type='submit'>COMMENT</button>
-                            <img src={icon} alt="comment icon" className='comment__logo' />
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            <ul className='comment__list'>
-                {videoSelected.comments.sort((a, b) => {
-                    return b.timestamp - a.timestamp
-                }).map((element) => {
-                    <li className='comment__container'>
-                        <div className='comment__lefts'>
-                            <img className='comment__images' src={grey} alt="empty avatar" />
-                        </div>
-
-                        <div className='comment__rights'>
-                            <div className='comment__namedate'>
-                                <span className='comment__name'>{commentInfo.name}</span>
-                                <span className='comment__date'>{new Date(commentInfo.timestamp).toLocaleDateString('en-US')}</span>
+                    <div className='comment__left'>
+                        <h2>JOIN THE CONVERSATION</h2>
+                        <form className='comment__form'>
+                            <div className='comment__box'>
+                                <input className='comment__input' type="text" placeholder='Add a new comment' name="comment" />
                             </div>
-                            <p>
-                                {commentInfo.comment}
-                            </p>
-                        </div>
-                    </li>
-                })
-            }
-            </ul>
-        </div>
 
+                            <div className='comment__button'>
+                                <button type='submit'>COMMENT</button>
+                                <img src={icon} alt="comment icon" className='comment__logo' />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <ul className='comment__list'>
+                    {videoSelected.comments?.map((comment) => {
+                            return(
+                                <li key={comment.id} className='comment__container'>
+                                    <div className='comment__lefts'>
+                                        <img className='comment__images' src={grey} alt="empty avatar" />
+                                    </div>
+
+                                    <div className='comment__rights'>
+                                        <div className='comment__namedate'>
+                                            <span className='comment__name'>{comment.name}</span>
+                                            <span className='comment__date'>{new Date(comment.timestamp).toLocaleDateString('en-US')}</span>
+                                        </div>
+                                        <p>
+                                            {comment.comment}
+                                        </p>
+                                    </div>
+                                </li>
+                            );
+                    })}
+                </ul>
+            </div>
+        </>
     );
 }
 
